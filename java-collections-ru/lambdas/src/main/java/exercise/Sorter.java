b75fa3e44a79942f9fd76ade7498df41bf79d3f8
@hexlet/cli <support@hexlet.io>
2022-12-04 17:09:04 +0300

    submit java-collections-ru/lambdas

---
+++ java-collections-ru/lambdas/src/main/java/exercise/Sorter.java
@@ -0,0 +1,29 @@
+package exercise;
+
+// BEGIN
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Sorter {
+    public static List<String> takeOldestMans(List<Map<String,String>> users) {
+        var result = users.stream()
+                .filter(user -> user.get("gender").equals("male"))
+                .map(user -> user.get("name"))
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    public static void main(String[] args) {
+        List<Map<String, String>> users = List.of(Map.of("name", "Vladimir Nikolaev", "birthday", "1990-12-27", "gender", "male"),
+                Map.of("name", "Andrey Petrov", "birthday", "1989-11-23", "gender", "male"),
+                Map.of("name", "Anna Sidorova", "birthday", "1996-09-09", "gender", "female"),
+                Map.of("name", "John Smith", "birthday", "1989-03-11", "gender", "male"),
+                Map.of("name", "Vanessa Vulf", "birthday", "1985-11-16", "gender", "female"),
+                Map.of("name", "Alice Lucas", "birthday", "1986-01-01", "gender", "female"),
+                Map.of("name", "Elsa Oscar", "birthday", "1970-03-10", "gender", "female")
+        );
+        System.out.println(Sorter.takeOldestMans(users));
+    }
+}
+// END
